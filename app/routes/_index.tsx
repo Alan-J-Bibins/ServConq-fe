4331796dfe6a52f8c834869df28ca52f604f2f70
Alan-J-Bibins <alanjbibins@gmail.com>
2025-09-05 20:10:27 +0530

    fix: TailwindCss-Motion not being setup properly

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,10 @@
-export default function _index() {
+export default function Page() {
     return (
-        <div>Index Page</div>
+        <div className="flex justify-center items-center h-screen w-full">
+            <h1 className="text-9xl motion-opacity-in-15 motion-translate-y-in-50">
+                ServConq
+            </h1>
+        </div>
     )
 }
 
