f8bfe5ab44b33a3b3d26ccb70ebcee368a669ab9
Alan-J-Bibins <alanjbibins@gmail.com>
2025-09-24 17:05:17 +0530

    feat: Setup colorscheme

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,10 @@
 export default function Page() {
     return (
-        <div className="flex justify-center items-center h-screen w-full">
-            <h1 className="text-9xl motion-opacity-in-15 motion-translate-y-in-50">
+        <div className="flex flex-col justify-center items-center h-screen w-full">
+            <h1 className="text-5xl motion-opacity-in motion-translate-y-in">
+                Welcome To
+            </h1>
+            <h1 className="text-9xl gradientText text-shadow-xl text-shadow-primary/45 motion-delay-1000 motion-opacity-in motion-translate-y-in">
                 ServConq
             </h1>
         </div>
