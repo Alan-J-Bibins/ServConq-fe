124dc28af74f54c0cc56c45caa18a5430d038974
Alan-J-Bibins <alanjbibins@gmail.com>
2025-10-23 14:05:43 +0300

    feat: Completed sidebar and added more routes

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,16 +1,29 @@
+import { HardDrive, LayoutDashboard, UserRoundPen, UsersRound, type LucideIcon } from "lucide-react"
 import { href, NavLink } from "react-router"
 
 export default function Sidebar() {
     return (
-        <aside className="h-full border border-transparent border-r-secondary">
-            Sidebar
+        <aside className="flex flex-col justify-start items-center p-4 gap-4
+            h-full border border-transparent border-r-secondary">
+            <SidebarItem to="/center" Icon={HardDrive} />
+            <SidebarItem to="/dashboard" Icon={LayoutDashboard} />
+            <SidebarItem to="/teams" Icon={UsersRound} />
+            <SidebarItem to="/profile" Icon={UserRoundPen} />
         </aside>
     )
 }
 
-function SidebarItem({ to }: { to: string }) {
+function SidebarItem({ to, Icon }: { to: string, Icon: LucideIcon }) {
     return (
-        <NavLink to={to}>
+        <NavLink to={to} className={({ isActive }) => `p-2 rounded-2xl transition-all border
+            ${isActive
+                ?
+                "text-primary bg-gradient-to-b from-secondary/20 to-primary/20 border-t-secondary/40 \
+                border-b-accent border-x-transparent shadow-custom shadow-accent/25 \
+                hover:shadow-accent/40"
+                : "text-secondary border-transparent hover:border-secondary/40 hover:text-primary hover:bg-secondary/20"}
+            `}>
+            <Icon size={24} />
         </NavLink>
     )
 }
